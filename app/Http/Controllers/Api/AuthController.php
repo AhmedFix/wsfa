798ccf48820da625c7aee0b24a457e14c1389a16
Alshrk <email>
2022-02-17 23:05:02 +0200

    solve the base

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,13 +2,12 @@
    
 namespace App\Http\Controllers\API;
    
-use Illuminate\Notifications\Notificatio;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use App\Http\Controllers\API\BaseController as BaseController;
+use App\Http\Controllers\Api\BaseController as BaseController;
 use App\Models\User;
 
    
